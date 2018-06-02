7488243e184aaec2640ab5ad6377c957e3ccd807
Showsay You <akizuki.c10.l65@gmail.com>
2018-06-02 14:19:05 +0900

    Feature: Define ConsoleInputExceptionInterface

---
+++ src/Exceptions/ConsoleInputExceptionInterface.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Koncept\ConsoleApp\Exceptions;
+
+use Strict\Collection\Vector\Scalar\Vector_string;
+
+
+/**
+ * [Interface] ConsoleInputExceptionInterface
+ *
+ * @author Showsay You <akizuki.c10.l65@gmail.com>
+ * @copyright 2018 Koncept. All Rights Reserved.
+ * @package koncept/console-application
+ * @since v1.0.0
+ */
+interface ConsoleInputExceptionInterface
+{
+    /**
+     * Explain the reason why this exception is thrown.
+     *
+     * @return Vector_string
+     */
+    public function explain(): Vector_string;
+}
